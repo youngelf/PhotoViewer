237313457f6d253059e64191a85615b67e2a3973
Vikram Aggarwal <vikram@eggwall.com>
2017-12-29 22:14:44 -0800

    DownloadManager and Gestures work
    Image downloading added using Download Manager.  It downloads an image
    and puts it in the default download directory, so it shows up in the Downloads
    program.
    
    Swiping left and right also works using a GestureDetector, though I am not sure
    I will keep the mechanic.
    
    Using the res/drawable/... directory for images right now.
    
    TODO: Add a file traversal to pick images from disk.
    
    TODO: Onscreen buttons that show next/previous because gesture are difficult
    to detect.  And these buttons should autohide.
    
    TODO: Make it a fullscreen application.

--- mobile/src/main/java/com/eggwall/android/photoviewer/MainActivity.java
+++ mobile/src/main/java/com/eggwall/android/photoviewer/MainActivity.java
@@ -1,8 +1,22 @@
 package com.eggwall.android.photoviewer;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.TouchDelegate;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,21 +26,125 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.SYSTEM_UI_FLAG_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+import static android.view.View.SYSTEM_UI_FLAG_LOW_PROFILE;
+import static android.view.View.VISIBLE;
 
 /**
  * Create a Photo viewer by default.  This screen should show an image by default, and allow the
  * user to change them using the navigation bar.
  */
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+// TODO: Set a gesture handler to swipe left/right for next image.
+// TODO: Put more than one image in.
+// TODO: Implement a broadcast receiver when a zip is downloaded.
+// TODO: Unzip a file.
+// TODO: Allow traversing existing file structure.
+// TODO: Delete oldest file: LRU cache.
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        View.OnSystemUiVisibilityChangeListener {
+
+    GestureDetectorCompat mDetector;
+
+    // All the images we will display
+    int[] drawables = {
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_gallery,
+            R.drawable.ic_menu_manage,
+            R.drawable.ic_menu_send,
+            R.drawable.ic_menu_share,
+            R.drawable.ic_menu_slideshow};
+    int currentDrawable = 0;
+
+    long mRequestId;
+
+    private BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            //check if the broadcast message is for our enqueued download
+            long referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+
+            if(referenceId == mRequestId) {
+                Toast toast = Toast.makeText(MainActivity.this, "Image Download Complete", Toast
+                        .LENGTH_LONG);
+                Log.d("MainActivity", "Image download complete");
+                toast.show();
+            }
+        }
+    };
+
+    private class FlingDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            Log.d("MainActivity", "Scroll (" + distanceX + ", " + distanceY + ")");
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+
+        @Override
+        public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+            if (v < -2000) {
+                Log.d("MainActivity", "PREV image");
+                updateImage(-1);
+            } else if (v > 2000) {
+                Log.d("MainActivity", "NEXT image");
+                updateImage(+1);
+            }
+            Log.d("MainActivity", "Listener detected fling (" + v + " , " + v1 + ").");
+            return true;
+
+        }
+    }
+
+    String location = "http://gallery.eggwall.com/gallery_23_Sept_Just_Home/_DSC8193.jpg";
+    private GestureDetector.OnGestureListener mGestureListener = new FlingDetector();
+
+    int mLastSystemUiVis = 0;
+    DrawerLayout mDrawer;
+    Toolbar mToolbar;
+    int mBaseSystemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+    private View.OnTouchListener mDelegate = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mDetector.onTouchEvent(motionEvent);
+            // Consume the events.  This is required to detect flings, otherwise they get
+            // detected as long press events.
+            return true;
+        }
+    };
+
+    private void updateImage(int offset) {
+        currentDrawable += offset;
+        if (currentDrawable < 0) {
+            currentDrawable = drawables.length - 1;
+        }
+        if (currentDrawable >= drawables.length) {
+            currentDrawable = 0;
+        }
+        image.setImageResource(drawables[currentDrawable]);
+    }
+
+    AppCompatImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        // Make the main view full screen, and listen for System UI visibility changes
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer.setOnSystemUiVisibilityChangeListener(this);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,15 +154,71 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        DownloadManager dMan = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(location));
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mRequestId = dMan.enqueue(request);
+        IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(downloadReceiver, filter);
+
+        image = (AppCompatImageView) findViewById(R.id.photoview);
+        image.setOnTouchListener(mDelegate);
+
+        mDetector = new GestureDetectorCompat(this, mGestureListener);
+        ;
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    public void onSystemUiVisibilityChange(int visibility) {
+        // Detect when we go out of low-profile mode, to also go out
+        // of full screen.  We only do this when the low profile mode
+        // is changing from its last state, and turning off.
+        int diff = mLastSystemUiVis ^ visibility;
+        mLastSystemUiVis = visibility;
+        if ((diff & SYSTEM_UI_FLAG_LOW_PROFILE) != 0
+                && (visibility & SYSTEM_UI_FLAG_LOW_PROFILE) == 0) {
+            setNavVisibility(true);
+        }
+    }
+
+
+    Runnable mNavHider = new Runnable() {
+        @Override
+        public void run() {
+            setNavVisibility(false);
+        }
+    };
+
+    void setBaseSystemUiVisibility(int visibility) {
+        mBaseSystemUiVisibility = visibility;
+    }
+
+    void setNavVisibility(boolean visible) {
+        int newVis = mBaseSystemUiVisibility;
+        if (!visible) {
+            newVis |= SYSTEM_UI_FLAG_LOW_PROFILE | SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+        final boolean changed = newVis == mDrawer.getSystemUiVisibility();
+
+        // Unschedule any pending event to hide navigation if we are
+        // changing the visibility, or making the UI visible.
+        if (changed || visible) {
+            Handler h = mDrawer.getHandler();
+            if (h != null) {
+                h.removeCallbacks(mNavHider);
+            }
+        }
+
+        // Set the new desired visibility.
+        mDrawer.setSystemUiVisibility(newVis);
+        mToolbar.setVisibility(visible ? VISIBLE : INVISIBLE);
     }
 
     @Override
@@ -102,5 +276,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
