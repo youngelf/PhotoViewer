f2f49cd56c107142e86a7695bc77711c89fc6207
Vikram Aggarwal <vikram@eggwall.com>
2018-01-01 23:06:52 -0800

    Got fullscreen working
    It is sticky, and somewhat aggressive.  I will leave it as-is, and get
    the filesystem traversal working tomorrow.  Once file handling in good shape,
    I'll return to fixing the UI so it looks pretty.

--- mobile/src/main/java/com/eggwall/android/photoviewer/MainActivity.java
+++ mobile/src/main/java/com/eggwall/android/photoviewer/MainActivity.java
@@ -25,10 +25,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewPropertyAnimator;
 import android.view.WindowManager;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.Toast;
 
 import java.io.File;
@@ -456,6 +453,19 @@ public class MainActivity extends AppCompatActivity implements
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
